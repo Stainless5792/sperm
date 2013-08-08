0d5f23f8e393f6835b66e7b4620e4258033fbab0
dirtysalt <dirtysalt1987@gmail.com>
2013-08-08 17:44:14 +0800

    [gc]commit

--- code/java/playboard/src/main/java/com/dirlt/java/playboard/HashAlgorithm.java
+++ code/java/playboard/src/main/java/com/dirlt/java/playboard/HashAlgorithm.java
@@ -1,7 +1,7 @@
 package com.dirlt.java.playboard;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,18 +12,16 @@ import java.security.NoSuchAlgorithmException;
  */
 public class HashAlgorithm {
     public static int hash(String umid) {
-        MessageDigest digest = null;
-        try {
-            digest = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            return 0;
-        }
-        digest.update(umid.getBytes());
-        byte[] values = digest.digest();
+        byte[] values = umid.toLowerCase().getBytes();
         int code = 0;
         for (byte b : values) {
-            code = (code << 8) + (int) b;
+            int v = (int) b;
+            if (v >= (int) '0' && v <= (int) '9') {
+                v -= (int) '0';
+            } else if (v >= (int) 'a' && v <= (int) 'f') {
+                v -= (int) 'a' + 10;
+            }
+            code = (code << 4) + v;
         }
         if (code < 0) {
             code = -code;
@@ -43,10 +41,22 @@ public class HashAlgorithm {
     }
 
     public static void main(String[] args) throws Exception {
-        args = "hello world yes no fuck".split(" ");
-        for (String arg : args) {
-            System.out.println("'" + arg + "' hash code = " + hash(arg));
-            System.out.println(hash(arg) >> 8);
+        int max_zeros[] = new int[1 << 10];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String umid = reader.readLine();
+            if (umid == null) {
+                break;
+            }
+            int h = hash(umid);
+            int bucket = h % max_zeros.length;
+            int bucket_hash = h / max_zeros.length;
+            max_zeros[bucket] = Math.max(max_zeros[bucket], trailing_zero_count(bucket_hash));
         }
+        for (int i = 0; i < max_zeros.length; i++) {
+            System.out.print(Integer.toString(max_zeros[i]));
+            System.out.print(" ");
+        }
+        System.out.println("");
     }
 }
